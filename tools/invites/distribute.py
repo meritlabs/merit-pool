6b4d428094f124fe95d87dbeb15cbcf96bf3f8d2
retsediv <retsediv1@gmail.com>
2018-07-20 19:17:42 +0300

    Distribuyte invites using merit-cli

--- tools/invites/distribute.py
+++ tools/invites/distribute.py
@@ -1,4 +1,6 @@
 import argparse
+import subprocess
+import config
 
 # Parse arguments
 ap = argparse.ArgumentParser()
@@ -8,7 +10,21 @@ args = vars(ap.parse_args())
 
 
 def distribute_invites():
-    pass
+    with open(args["filename"], "r") as f:
+        for i in f.readlines():
+            entry = i.strip().split(",")
+
+            command = [
+                'merit-cli',
+                '-conf={}'.format(config.MERIT_CONF),
+                '-datadir={}'.format(config.MERIT_DATA_DIR),
+                'inviteaddress',
+                '"{}"'.format(entry[0]),
+                '{}'.format(entry[1])
+            ]
+
+            print("COMMAND: ", " ".join(command))
+            print(subprocess.check_output(command))
 
 
 if __name__ == "__main__":
