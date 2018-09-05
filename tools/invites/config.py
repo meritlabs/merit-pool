2ebf08d3f112ac92e3767b9a888606c059130c5a
Maxim Khailo <max@merit.me>
2018-09-05 10:58:51 -0700

    Removing silly config examples.

--- tools/invites/config.py
+++ tools/invites/config.py
@@ -1,11 +1,11 @@
 # MYSQL config
-DB_MYSQL_USER = "root"
-DB_MYSQL_USERPASS = "1234"
+DB_MYSQL_USER = ""
+DB_MYSQL_USERPASS = ""
 DB_MYSQL_PORT = 3306
-DB_MYSQL_HOST = "localhost"
-DB_MYSQL_NAME = "mempool_live"
+DB_MYSQL_HOST = ""
+DB_MYSQL_NAME = ""
 
 # meritd config
-MERIT_CONF = "/home/andrew/merit-livenet/merit.conf"
-MERIT_DATA_DIR = "/home/andrew/merit-livenet"
-MERIT_MEMPOOL_ADDRESS = "MEpuLtAoXayZAst1EYtnrTzjc9bfqf49kj"
+MERIT_CONF = ""
+MERIT_DATA_DIR = ""
+MERIT_MEMPOOL_ADDRESS = ""
